47e4d711b37ac9ab1bf5c25821c57dafce298961
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-23 14:41:30 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener("submit", function(event){
+    event.preventDefault()
+    //event.target.new-task-description.value
+    let get = document.querySelector(`#new-task-description`).value
+    miniProject(get)
+
+  })
+
 });
+function miniProject (list) {
+  let x = document.createElement('p')
+  
+  let orp = document.createElement('button')
+  orp.textContent = "x"
+  orp.addEventListener("click", handler)
+  x.textContent = list
+  x.appendChild(orp)
+  document.querySelector(`#list`).appendChild(x)
+}
+function handler(event){
+  event.target.parentNode.remove()
+}
